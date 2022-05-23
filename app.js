9d174a6da992f38c53c1791e4c4f6853b8452110
Abu <huzaifa@gmail.com>
2022-05-24 00:00:00 +0500

    once again

---
+++ app.js
@@ -0,0 +1,16 @@
+function myfunction () {
+    document.getElementById('one').style.color='green';
+    document.getElementById('one').style.fontSize='35px';
+}
+document.getElementById('mm').innerHTML=5 + 6;
+
+// document.write(
+//     "hello world"
+// )
+// alert(
+//     "hey there"
+// )
+
+let x;
+x = 8 + 8;
+document.write(x)
